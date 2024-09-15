a94912016aeb666e5a418363c4db6f5a98e85838
hector <hsdsegura6@gmail.com>
2024-09-14 19:39:58 -0500

    configuracion de area de trabajo

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import UserController from '../controller/user'
+// import UserController from '../controller/user'
 import { Client } from 'pg'
 // const { Client } = require('pg');
 
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  // UserController. 
+  // UserController
 })
 app.post('/register', (req, res) => { })
 app.post('/logout', (req, res) => { })
